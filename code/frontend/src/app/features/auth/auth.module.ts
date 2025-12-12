bfde5f83ea053ce723be8a31bfb4a04917d25122
Ahmad-Ajm <ahaj82@hotmail.com>
2025-12-12 22:07:13 +0300

    إنشاء وحدة المصادقة (AuthModule) للمكونات الأساسية

---
+++ code/frontend/src/app/features/auth/auth.module.ts
@@ -0,0 +1,61 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+// Angular Material Modules
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+// Components
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+
+// Routes
+import { authRoutes } from './auth.routes';
+
+@NgModule({
+  declarations: [
+    RegisterComponent,
+    LoginComponent,
+    ProfileComponent,
+    ForgotPasswordComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(authRoutes),
+    
+    // Angular Material Modules
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule,
+    MatTabsModule,
+    MatDividerModule,
+    MatTooltipModule
+  ],
+  exports: [
+    RegisterComponent,
+    LoginComponent,
+    ProfileComponent,
+    ForgotPasswordComponent
+  ]
+})
+export class AuthModule { }
