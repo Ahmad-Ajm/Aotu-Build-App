b12a55336c8a3a5eaa999c38a0997922e566286b
Ahmad-Ajm <ahaj82@hotmail.com>
2025-12-14 12:36:00 +0300

    FEAT-CV-CREATION: Create Skill DTO

---
+++ code/backend/CVSystem.Application.Contracts/CVs/Dtos/SkillForCvDto.cs
@@ -0,0 +1,9 @@
+namespace CVSystem.CVs.Dtos
+{
+    public class SkillForCvDto
+    {
+        public string Name { get; set; }
+        public string? Level { get; set; }
+        public int? Years { get; set; }
+    }
+}
