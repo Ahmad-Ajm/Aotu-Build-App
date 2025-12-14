0e1d15ee1b675de44317f3e5aed4e0fa35748e72
Ahmad-Ajm <ahaj82@hotmail.com>
2025-12-14 12:35:53 +0300

    FEAT-CV-CREATION: Create WorkExperience DTO

---
+++ code/backend/CVSystem.Application.Contracts/CVs/Dtos/WorkExperienceForCvDto.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace CVSystem.CVs.Dtos
+{
+    public class WorkExperienceForCvDto
+    {
+        public string JobTitle { get; set; }
+        public string Company { get; set; }
+        public string? Location { get; set; }
+        public string StartDate { get; set; }
+        public string? EndDate { get; set; }
+        public string? Description { get; set; }
+    }
+}
