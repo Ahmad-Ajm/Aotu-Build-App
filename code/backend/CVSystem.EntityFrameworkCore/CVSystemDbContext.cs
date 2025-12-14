88aba9b94927893f418bb63d12839ae92b1c7b2d
Ahmad-Ajm <ahaj82@hotmail.com>
2025-12-14 12:35:33 +0300

    FEAT-CV-CREATION: Add CV DbSet to DbContext

--- code/backend/CVSystem.EntityFrameworkCore/CVSystemDbContext.cs
+++ code/backend/CVSystem.EntityFrameworkCore/CVSystemDbContext.cs
@@ -1,49 +1,92 @@
-using CVSystem.Domain.Users;
 using Microsoft.EntityFrameworkCore;
+using Volo.Abp.AuditLogging.EntityFrameworkCore;
+using Volo.Abp.BackgroundJobs.EntityFrameworkCore;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.EntityFrameworkCore;
+using Volo.Abp.FeatureManagement.EntityFrameworkCore;
+using Volo.Abp.Identity.EntityFrameworkCore;
+using Volo.Abp.OpenIddict.EntityFrameworkCore;
+using Volo.Abp.PermissionManagement.EntityFrameworkCore;
+using Volo.Abp.SettingManagement.EntityFrameworkCore;
+using Volo.Abp.TenantManagement.EntityFrameworkCore;
+using CVSystem.Users;
+using CVSystem.CVs;
 
 namespace CVSystem.EntityFrameworkCore;
 
-public class CVSystemDbContext : DbContext
+[ReplaceDbContext(typeof(IIdentityDbContext))]
+[ReplaceDbContext(typeof(ITenantManagementDbContext))]
+public class CVSystemDbContext :
+    AbpDbContext<CVSystemDbContext>,
+    IIdentityDbContext,
+    ITenantManagementDbContext
 {
+    /* Add DbSet properties for your Aggregate Roots and Entities here. */
+
+    #region Entities from the modules
+
+    /* Inherited DbSets from Storybook modules: */
+    public DbSet<CV> CVs { get; set; }
+    public DbSet<AppUser> Users { get; set; }
+    public DbSet<UserProfile> UserProfiles { get; set; }
+
+    #endregion
+
     public CVSystemDbContext(DbContextOptions<CVSystemDbContext> options) : base(options)
     {
+
     }
 
-    public DbSet<User> Users => Set<User>();
-    public DbSet<UserProfile> UserProfiles => Set<UserProfile>();
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    protected override void OnModelCreating(ModelBuilder builder)
     {
-        base.OnModelCreating(modelBuilder);
+        base.OnModelCreating(builder);
 
-        modelBuilder.Entity<User>(b =>
+        /* Include modules to build your domain model.*/
+
+        builder.ConfigurePermissionManagement();
+        builder.ConfigureSettingManagement();
+        builder.ConfigureBackgroundJobs();
+        builder.ConfigureAuditLogging();
+        builder.ConfigureIdentity();
+        builder.ConfigureOpenIddict();
+        builder.ConfigureFeatureManagement();
+        builder.ConfigureTenantManagement();
+
+        /* Configure your own tables/entities inside here */
+
+        builder.Entity<AppUser>(b =>
         {
-            b.ToTable("Users");
-            b.HasKey(x => x.Id);
-            b.Property(x => x.UserName).HasMaxLength(100).IsRequired();
-            b.HasIndex(x => x.UserName).IsUnique();
-            b.Property(x => x.Email).HasMaxLength(255).IsRequired();
-            b.HasIndex(x => x.Email).IsUnique();
-            b.Property(x => x.PasswordHash).HasMaxLength(255).IsRequired();
-            b.Property(x => x.FullName).HasMaxLength(100).IsRequired();
-            b.Property(x => x.IsActive).HasDefaultValue(true);
-            b.Property(x => x.FailedLoginAttempts).HasDefaultValue(0);
-            b.Property(x => x.CreatedAt).HasDefaultValueSql("CURRENT_TIMESTAMP");
-            b.Property(x => x.UpdatedAt).HasDefaultValueSql("CURRENT_TIMESTAMP");
-
-            b.HasOne(x => x.Profile)
-                .WithOne(x => x.User)
-                .HasForeignKey<UserProfile>(x => x.UserId);
+            b.ToTable(AbpIdentityDbProperties.DbTablePrefix + "Users"); // Moved back to AbpIdentityDbProperties
+            b.ConfigureByConvention();
+            b.Property(x => x.CreationTime).HasColumnName(nameof(AppUser.CreationTime));
+            b.HasMany(x => x.UserProfiles).WithOne().HasForeignKey(x => x.UserId).IsRequired();
+            b.HasIndex(x => x.NormalizedUserName);
+            b.HasIndex(x => x.NormalizedEmail);
         });
 
-        modelBuilder.Entity<UserProfile>(b =>
+        builder.Entity<UserProfile>(b =>
         {
             b.ToTable("UserProfiles");
+            b.ConfigureByConvention();
             b.HasKey(x => x.Id);
-            b.Property(x => x.PhoneNumber).HasMaxLength(20);
-            b.Property(x => x.ProfileImageUrl).HasMaxLength(500);
-            b.Property(x => x.CreatedAt).HasDefaultValueSql("CURRENT_TIMESTAMP");
-            b.Property(x => x.UpdatedAt).HasDefaultValueSql("CURRENT_TIMESTAMP");
+            b.Property(x => x.FullName).IsRequired().HasMaxLength(UserProfileConsts.FullNameMaxLength);
+            b.HasOne<AppUser>().WithMany(x => x.UserProfiles).HasForeignKey(x => x.UserId).IsRequired();
+            b.HasIndex(x => x.UserId);
+        });
+
+        builder.Entity<CV>(b =>
+        {
+            b.ToTable("CVs");
+            b.ConfigureByConvention();
+            b.Property(x => x.Title).IsRequired().HasMaxLength(200);
+            b.Property(x => x.PersonalInfo).HasColumnType("jsonb");
+            b.Property(x => x.WorkExperience).HasColumnType("jsonb");
+            b.Property(x => x.Education).HasColumnType("jsonb");
+            b.Property(x => x.Skills).HasColumnType("jsonb");
+            b.Property(x => x.Template).HasMaxLength(50);
+            b.HasIndex(x => x.UserId);
+            b.HasIndex(x => x.IsPublic);
         });
     }
 }
